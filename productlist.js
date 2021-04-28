a50f526be18c051f89849b0776608579dd17bafb
Maria-Lavinia Otelea <otelea.lavi@gmail.com>
2021-04-28 09:11:22 +0200

    product and product list pages

---
+++ productlist.js
@@ -0,0 +1,32 @@
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+//function handleProductList(data) {
+  // console.log(data)
+  //data.forEach(showProduct);
+
+//function showProduct(product) {
+    //console.log(product);
+    //grab the template
+  
+    //const template = document.querySelector("#product").content;
+    //clone it
+    //const copy = template.cloneNode(true);
+    //change content
+    //copy.querySelector("h3").textContent = product.productname;
+    //copy.querySelector("a").href = `product.html?id=${product.id}`;
+    //copy.querySelector(".price").textContent = product.price + " DKK";
+    //copy.querySelector(
+       // "img.productimage"
+      //).src = ``;
+      //copy.querySelector("img.productimage").alt = product.productname;
+      //const parent = document.querySelector("main");
+      //append it
+      //parent.appendChild(copy);
+//}
+//}
