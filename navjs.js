1819ed1d5c3b36f4ccc406e2b31810967965c718
Agata Figura <agata.figura1@gmail.com>
2021-05-04 11:50:20 +0200

    .

---
+++ navjs.js
@@ -0,0 +1,32 @@
+const navToggle = document.querySelector(".nav-toggle");
+const categories = document.querySelector(".nav-categories");
+
+navToggle.addEventListener("click", function () {
+  categories.classList.toggle("show-categories");
+});
+
+const products = document.querySelector(".linkproducts");
+const links = document.querySelector(".productlinks");
+
+products.addEventListener("click", function () {
+  links.classList.toggle("show-linkss");
+});
+
+const collections = document.querySelector(".linkcollections");
+const linkss = document.querySelector(".collectionlinks");
+
+collections.addEventListener("click", function () {
+  linkss.classList.toggle("show-linkss");
+});
+
+const silfen = document.querySelector(".silfen");
+const silfenplay = document.querySelector(".silfenplay");
+
+silfen.addEventListener("click", function () {
+  //   window.location.href = "index.html";-------need to add this when other pages ready
+  silfen.classList.toggle("blue");
+});
+silfenplay.addEventListener("click", function () {
+  //   window.location.href = "index.html";-------need to add this when other pages ready
+  silfenplay.classList.toggle("blue");
+});
