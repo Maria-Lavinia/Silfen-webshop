7b0fa3240d64827e210a7895b6f6565f27446f94
Caroline Wulff <carolineisinchina@gmail.com>
2021-05-04 10:16:17 +0200

    checkout media queries

--- basket.js
+++ basket.js
@@ -1,15 +1,12 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("_id");
 
-fetch(
-  "https://kea21s-5d8f.restdb.io/rest/product/" + id + "?fetchchildren=true",
-  {
-    method: "GET",
-    headers: {
-      "x-apikey": "606d5ed3f553500431007503",
-    },
-  }
-)
+fetch("https://kea21s-5d8f.restdb.io/rest/product/" + id, {
+  method: "GET",
+  headers: {
+    "x-apikey": "606d5ed3f553500431007503",
+  },
+})
   .then((res) => res.json())
   .then((data) => showProduct(data));
 
