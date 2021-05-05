d4a33b1e21ad0cd25178dc19f4a149eb56a27fca
Agata Figura <agata.figura1@gmail.com>
2021-05-05 20:41:52 +0200

    final version of landing pages

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ function submitForm() {
     
     document.getElementById("name").value="";
     document.getElementById("email").value="";
-}
+};
+
+window.addEventListener("load", slide);
+
+var mqls = [
+    window.matchMedia("(min-width: 1024px)"),
+    window.matchMedia("(min-width: 768px) and (max-width: 1023px"),
+    window.matchMedia("(max-width: 767px)")
+];
+
+function slide() {
+
+if (mqls[0].matches) {
+
+console.log("web");
 
 const flightPath1 = {
     curviness: 0,
@@ -20,7 +34,7 @@ const flightPath2 = {
     curviness: 0,
     autoRotate: false,
     values: [
-        {x: -400, y:0}
+        {x: -350, y:0}
     ]
 };
 
@@ -141,3 +155,282 @@ const scene4 = new ScrollMagic.Scene({
 
 .setTween(tween4)
 .addTo(controller4);
+
+} if (mqls[1].matches) {
+
+    console.log("tablet");
+
+    const flightPath1 = {
+        curviness: 0,
+        autoRotate: false,
+        values: [
+          {x: 380, y: 0}
+        ]
+    };
+    
+    const flightPath2 = {
+        curviness: 0,
+        autoRotate: false,
+        values: [
+            {x: -380, y:0}
+        ]
+    };
+    
+    const flightPath3 = {
+        curviness: 0,
+        autoRotate: false,
+        values: [
+            {x: 0, y:-600}
+        ]
+    };
+
+    const tween = new TimelineLite();
+
+tween.add(
+    TweenLite.to('#yellow-splashes', 0.5, {
+        bezier: flightPath1,
+        ease: Power1.easeInOut
+    }),
+    "first"
+);
+
+tween.add(
+    TweenLite.to('#blue-splashes', 0.5, {
+        bezier: flightPath2,
+        ease: Power1.easeInOut,
+    }),
+    "first"
+);
+
+const controller1 = new ScrollMagic.Controller();
+
+const scene1 = new ScrollMagic.Scene({
+    triggerElement: '.background-video',
+    duration: 600,
+    triggerHook: 0
+
+})
+
+.setTween(tween)
+.addTo(controller1);
+
+const tween2 = new TimelineLite();
+
+tween2.add(
+    TweenLite.to('#green-splashes', 1, {
+        bezier: flightPath1,
+        ease: Power1.easeInOut
+    }),
+    "second"
+);
+
+tween2.add(
+    TweenLite.to('#purple-splashes', 1, {
+        bezier: flightPath2,
+        ease: Power1.easeInOut
+    }),
+    "second"
+);
+
+
+const controller2 = new ScrollMagic.Controller();
+
+const scene2 = new ScrollMagic.Scene({
+    triggerElement: '.section1',
+    duration: 600,
+    triggerHook: 0
+})
+
+.setTween(tween2)
+.addTo(controller2);
+
+const tween3 = new TimelineLite();
+
+tween3.add(
+    TweenLite.to('#yellow-splashes2', 1, {
+        bezier: flightPath2,
+        ease: Power1.easeInOut
+    }),
+    "third"
+);
+
+tween3.add(
+    TweenLite.to('#blue-splashes2', 1, {
+        bezier: flightPath1,
+        ease: Power1.easeInOut
+    }),
+    "third"
+);
+
+
+const controller3 = new ScrollMagic.Controller();
+
+const scene3 = new ScrollMagic.Scene({
+    triggerElement: '.section2',
+    duration: 600,
+    triggerHook: 0
+})
+
+.setTween(tween3)
+.addTo(controller3);
+
+const tween4 = new TimelineLite();
+
+tween4.add(
+    TweenLite.to('.section4', 1, {
+        bezier: flightPath3,
+        ease: Power1.easeInOut
+    }),
+);
+
+const controller4 = new ScrollMagic.Controller();
+
+const scene4 = new ScrollMagic.Scene({
+    triggerElement: '.section3',
+    duration: 1000,
+    triggerHook: 0
+})
+
+.setTween(tween4)
+.addTo(controller4);
+}
+
+
+} if (mqls[2].matches) {
+
+    console.log("mobile");
+
+    const flightPath1 = {
+        curviness: 0,
+        autoRotate: false,
+        values: [
+          {x: 250, y: 0}
+        ]
+    };
+    
+    const flightPath2 = {
+        curviness: 0,
+        autoRotate: false,
+        values: [
+            {x: -250, y:0}
+        ]
+    };
+    
+    const flightPath3 = {
+        curviness: 0,
+        autoRotate: false,
+        values: [
+            {x: 0, y:-600}
+        ]
+    };
+
+    const tween = new TimelineLite();
+
+tween.add(
+    TweenLite.to('#yellow-splashes', 0.5, {
+        bezier: flightPath1,
+        ease: Power1.easeInOut
+    }),
+    "first"
+);
+
+tween.add(
+    TweenLite.to('#blue-splashes', 0.5, {
+        bezier: flightPath2,
+        ease: Power1.easeInOut,
+    }),
+    "first"
+);
+
+const controller1 = new ScrollMagic.Controller();
+
+const scene1 = new ScrollMagic.Scene({
+    triggerElement: '.video-wrapper',
+    duration: 400,
+    triggerHook: 0
+
+})
+
+.setTween(tween)
+.addTo(controller1);
+
+const tween2 = new TimelineLite();
+
+tween2.add(
+    TweenLite.to('#green-splashes', 1, {
+        bezier: flightPath1,
+        ease: Power1.easeInOut
+    }),
+    "second"
+);
+
+tween2.add(
+    TweenLite.to('#purple-splashes', 1, {
+        bezier: flightPath2,
+        ease: Power1.easeInOut
+    }),
+    "second"
+);
+
+
+const controller2 = new ScrollMagic.Controller();
+
+const scene2 = new ScrollMagic.Scene({
+    triggerElement: '.section1',
+    duration: 400,
+    triggerHook: 0
+})
+
+.setTween(tween2)
+.addTo(controller2);
+
+const tween3 = new TimelineLite();
+
+tween3.add(
+    TweenLite.to('#yellow-splashes2', 1, {
+        bezier: flightPath2,
+        ease: Power1.easeInOut
+    }),
+    "third"
+);
+
+tween3.add(
+    TweenLite.to('#blue-splashes2', 1, {
+        bezier: flightPath1,
+        ease: Power1.easeInOut
+    }),
+    "third"
+);
+
+
+const controller3 = new ScrollMagic.Controller();
+
+const scene3 = new ScrollMagic.Scene({
+    triggerElement: '.section2',
+    duration: 400,
+    triggerHook: 0
+})
+
+.setTween(tween3)
+.addTo(controller3);
+
+const tween4 = new TimelineLite();
+
+tween4.add(
+    TweenLite.to('.section4', 1, {
+        bezier: flightPath3,
+        ease: Power1.easeInOut
+    }),
+);
+
+const controller4 = new ScrollMagic.Controller();
+
+const scene4 = new ScrollMagic.Scene({
+    triggerElement: '.section3',
+    duration: 1000,
+    triggerHook: 0
+})
+
+.setTween(tween4)
+.addTo(controller4);
+}
